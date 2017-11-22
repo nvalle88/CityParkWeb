82f93e55ba721e38a5ca3d1ed16ef7f1326314d8
Néstor Valle <nestorvalle880416@gmail.com>
2017-11-22 08:00:24 -0600

    Camvios a MVC5

---
+++ CityParkWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CityParkWeb.Startup))]
+namespace CityParkWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
